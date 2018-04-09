80b1f84c0af494635d6c1ada6c0254806d61e5c4
AndreTheGiant <you@example.com>
2018-04-09 11:30:14 +0000

    4th commit

--- myphp.php
+++ myphp.php
@@ -1,6 +1,6 @@
 <?php
 echo "HELO, is it me your looking for, have i told" "</br>" ;
-echo "Hello IP:" . $_SERVER['REMOTE_ADDR'] . "</br>";
+echo "Hello IP is it me your looking for:" . $_SERVER['REMOTE_ADDR'] . "</br>";
 echo "You are served by the server IP: <b>" . $_SERVER['SERVER_ADDR'] . "</b> </br>";
 $datetime = date("jF,Y,g:ia");
 echo "on: <b>" . $datetime . "</b>";
